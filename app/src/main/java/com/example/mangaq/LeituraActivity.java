9fc3c3a91db42df0b088b4c28c93e72f51bd9f27
opet <opet@Academico.edu>
2019-11-08 22:11:09 -0300

    Adição de leitura dinâmica

--- app/src/main/java/com/example/mangaq/LeituraActivity.java
+++ app/src/main/java/com/example/mangaq/LeituraActivity.java
@@ -3,39 +3,76 @@ package com.example.mangaq;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.pm.ActivityInfo;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.ScrollView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class LeituraActivity extends AppCompatActivity {
-    private int[] a = {R.drawable.a1, R.drawable.a2, R.drawable.a3, R.drawable.a4, R.drawable.a5, R.drawable.a6};
-    private int[] b = {R.drawable.b1, R.drawable.b2, R.drawable.b3, R.drawable.b4, R.drawable.b5, R.drawable.b6};
+    private ArrayList<Integer> a = new ArrayList<Integer>();
+    private ArrayList<Integer> b = new ArrayList<Integer>();
+    private ImageView imageView1, imageView2;
     private Button choice1, choice2;
-    private ListView history;
     private int pointer = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leitura);
-        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 
-        this.history = findViewById(R.id.historyViewList);
         this.choice1 = findViewById(R.id.btnEscolha1);
         this.choice2 = findViewById(R.id.btnEscolha2);
+
+        this.choice1.setText("A");
+        this.choice2.setText("B");
+
+        this.imageView1 = findViewById(R.id.image1);
+        this.imageView2 = findViewById(R.id.image2);
+
+        a.add(R.drawable.a1);
+        a.add(R.drawable.a2);
+        a.add(R.drawable.a3);
+        a.add(R.drawable.a4);
+        a.add(R.drawable.a5);
+        a.add(R.drawable.a6);
+        b.add(R.drawable.b1);
+        b.add(R.drawable.b2);
+        b.add(R.drawable.b3);
+        b.add(R.drawable.b4);
+        b.add(R.drawable.b5);
+        b.add(R.drawable.b6);
     }
 
     public void renderNextChoice(View view) {
+        TextView editText = findViewById(R.id.makeChoice);
+        editText.setVisibility(View.GONE);
         long choice = Integer.parseInt(view.getTag().toString(), 10);
-        loadListContent();
+        loadListContent(choice);
     }
 
-    public void loadListContent() {
-        //ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.list_content, ["asd", "ewew"]);
-        //history.setAdapter(adapter);
-
+    public void loadListContent(long choice) {
+        if (this.pointer < 6) {
+            if (choice == 1) {
+                imageView1.setImageResource(a.get(pointer));
+                imageView2.setImageResource(a.get(pointer + 1));
+            } else {
+                imageView1.setImageResource(b.get(pointer));
+                imageView2.setImageResource(b.get(pointer + 1));
+            }
+            this.pointer += 2;
+        }else {
+            Toast.makeText(this, "História finalizada", Toast.LENGTH_LONG).show();
+            finish();
+        }
     }
 }
